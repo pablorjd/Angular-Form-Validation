61e5973ca9ac81df42274c127b15af43f86fb779
pablorjd <pablo.jimenez2221@gmail.com>
2022-08-14 22:12:50 -0400

    Validacion Formulario reactivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(12),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
   name = 'Angular Form Validation' + VERSION.major;
+
+  get formControlName() {
+    return this.form.get('name');
+  }
+  get formControlEmail() {
+    return this.form.get('email');
+  }
+
+  sendForm() {
+    console.log(this.form.invalid);
+
+    if (this.form.invalid) {
+      Swal.fire(
+        'Los Campos deben ser Válidos',
+        'Se deben completar ambos campos',
+        'error'
+      );
+      return;
+    }
+    console.log(this.form);
+    Swal.fire({
+      title: 'Datos del Formulario',
+      text: `Formulario ${JSON.stringify(this.form.value)}`,
+    });
+  }
 }
